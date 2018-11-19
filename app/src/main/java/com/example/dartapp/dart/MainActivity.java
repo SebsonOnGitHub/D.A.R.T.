29ad6a25d43aa676aed41def7e0d8606b10b17e0
Clynch7 <anton.silfver@gmail.com>
2018-11-19 15:37:03 +0100

    Start of using Volley to send POST request to webserver. Currently not working yet, some errors and functionality left to implement

--- app/src/main/java/com/example/dartapp/dart/MainActivity.java
+++ app/src/main/java/com/example/dartapp/dart/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         SendDartHitTask sendDartHitTask = new SendDartHitTask();
-        sendDartHitTask.doInBackground();
+        sendDartHitTask.execute("Fisk");
     }
     public static Context getContext(){
         return MainActivity.context;
--- app/src/main/java/com/example/dartapp/dart/SendDartHitTask.java
+++ app/src/main/java/com/example/dartapp/dart/SendDartHitTask.java
@@ -1,21 +1,57 @@
 package com.example.dartapp.dart;
 
 import android.os.AsyncTask;
+import android.view.textclassifier.TextLinks;
 import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class SendDartHitTask extends AsyncTask<String, Void, String> {
     @Override
     protected String doInBackground(String[] params) {
-        return params[0];
+        String url = params[0];
+        JSONObject info = new JSONObject();
+        try{
+            info.put("score", params[1]);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        RequestQueue mRequestQueue;
+        mRequestQueue = Volley.newRequestQueue(MainActivity.getContext());
+        StringRequest request = new StringRequest(Request.Method.POST, url, new Response.Listener<String>(){
+
+            @Override
+            public void onResponse(String response) {
+
+            }
+        }, new Response.ErrorListener(){
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        }){
+
+        };
+        mRequestQueue.add(request);
+
+        return "Sucessfully sent dart information";
     }
 
 
     @Override
     protected void onPostExecute(String s) {
-        Toast.makeText(MainActivity.getContext(), "YAY A TOAST", Toast.LENGTH_LONG);
+        Toast toast = Toast.makeText(MainActivity.getContext(), "YAY A TOAST", Toast.LENGTH_LONG);
+        toast.show();
         super.onPostExecute(s);
-    }
-
-    public void doInBackground() {
     }
 }
